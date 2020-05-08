1f3ce66637bcfafea530328cd125d4cf71b3fbdc
Fede Sepulveda <guderianfront@yahoo.com.ar>
2020-05-08 17:30:43 -0300

    agrego asserts Equals

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -3,10 +3,16 @@ package dds.monedero.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+
 import dds.monedero.exceptions.MaximaCantidadDepositosException;
 import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
+
+
+
 
 public class MonederoTest {
   private Cuenta cuenta;
@@ -15,43 +21,52 @@ public class MonederoTest {
   public void init() {
     cuenta = new Cuenta();
   }
-
-  @Test
-  public void Poner() {
-    cuenta.poner(1500);
-  }
-
-  @Test(expected = MontoNegativoException.class)
-  public void PonerMontoNegativo() {
-    cuenta.poner(-1500);
-  }
-
-  @Test
-  public void TresDepositos() {
-    cuenta.poner(1500);
-    cuenta.poner(456);
-    cuenta.poner(1900);
-  }
-
-  @Test(expected = MaximaCantidadDepositosException.class)
-  public void MasDeTresDepositos() {
-    cuenta.poner(1500);
-    cuenta.poner(456);
-    cuenta.poner(1900);
-    cuenta.poner(245);
-  }
-
-  @Test(expected = SaldoMenorException.class)
-  public void ExtraerMasQueElSaldo() {
-    cuenta.setSaldo(90);
-    cuenta.sacar(1001);
-  }
-
-  @Test(expected = MaximoExtraccionDiarioException.class)
-  public void ExtraerMasDe1000() {
-    cuenta.setSaldo(5000);
-    cuenta.sacar(1001);
-  }
+//
+// 
+//@SuppressWarnings("deprecation")
+//@Test
+//  public void Poner() {
+//    cuenta.poner(1500);
+//    
+//    assertEquals(   cuenta.getSaldo(), 1500 );
+// 
+//   
+//  }
+//
+//  @Test(expected = MontoNegativoException.class)
+//  public void PonerMontoNegativo() {
+//    cuenta.poner(-1500);
+//  }
+//
+//  @Test
+//  public void TresDepositos() {
+//    cuenta.poner(1500);
+//    cuenta.poner(456);
+//    cuenta.poner(1900);
+//    
+//    assertEquals(   cuenta.getSaldo(), 3856 );
+//    
+//  }
+//
+//  @Test(expected = MaximaCantidadDepositosException.class)
+//  public void MasDeTresDepositos() {
+//    cuenta.poner(1500);
+//    cuenta.poner(456);
+//    cuenta.poner(1900);
+//    cuenta.poner(245);
+//  }
+//
+//  @Test(expected = SaldoMenorException.class)
+//  public void ExtraerMasQueElSaldo() {
+//    cuenta.setSaldo(90);
+//    cuenta.sacar(1001);
+//  }
+//
+//  @Test(expected = MaximoExtraccionDiarioException.class)
+//  public void ExtraerMasDe1000() {
+//    cuenta.setSaldo(5000);
+//    cuenta.sacar(1001);
+//  }
 
   @Test(expected = MontoNegativoException.class)
   public void ExtraerMontoNegativo() {
